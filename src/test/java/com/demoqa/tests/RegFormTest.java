a42166dfae64703e97e34b6b1b8498be83c3783c
Vladimir <none@none>
2023-12-04 18:12:11 -0500

    Allure report with attachments and remote config

--- src/test/java/com/demoqa/tests/RegFormTest.java
+++ src/test/java/com/demoqa/tests/RegFormTest.java
@@ -1,89 +1,112 @@
 package com.demoqa.tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.pages.RegistrationPage;
+import io.qameta.allure.Owner;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
+@Tag("RegFormTest")
+@Owner("Vladimir")
 public class RegFormTest extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
     TestData testData = new TestData();
 
     @Test
+    @Tag("Positive")
+    @DisplayName("Успешное заполнение всех полей регистрационной формы")
     void regFormFillAllSuccessTest() {
-
-        registrationPage.openPage()
-                .removeBanners()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setEmail(testData.userEmail)
-                .setGender(testData.gender)
-                .setMobile(testData.mobile)
-                .setDateOfBirth(testData.birthDay, testData.birthMonth, testData.birthYear)
-                .setSubjects(testData.selectedSubjects)
-                .setHobbies(testData.selectedHobbies)
-                .uploadPicture(testData.picture1)
-                .setAddress(testData.address1)
-                .setState(testData.state)
-                .setCity(testData.city)
-                .submitForm();
-
-        registrationPage.checkResultTableOpened()
-                .checkSubmittedValue("Student Name", testData.firstName + " " + testData.lastName)
-                .checkSubmittedValue("Student Email", testData.userEmail)
-                .checkSubmittedValue("Gender", testData.gender)
-                .checkSubmittedValue("Mobile", testData.mobile)
-                .checkSubmittedValue("Date of Birth", testData.birthDay + " " + testData.birthMonth + "," + testData.birthYear)
-                .checkSubmittedValue("Subjects", String.join(", ", testData.selectedSubjects))
-                .checkSubmittedValue("Hobbies", String.join(", ", testData.selectedHobbies))
-                .checkSubmittedValue("Picture", testData.picture1)
-                .checkSubmittedValue("Address", testData.address1)
-                .checkSubmittedValue("State and City", testData.state + " " + testData.city);
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> registrationPage.openPage());
+        step("Удаляем мешающие баннеры", () -> registrationPage.removeBanners());
+        step("Заполняем все поля регистрационной формы", () -> {
+            registrationPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setEmail(testData.userEmail)
+                    .setGender(testData.gender)
+                    .setMobile(testData.mobile)
+                    .setDateOfBirth(testData.birthDay, testData.birthMonth, testData.birthYear)
+                    .setSubjects(testData.selectedSubjects)
+                    .setHobbies(testData.selectedHobbies)
+                    .uploadPicture(testData.picture1)
+                    .setAddress(testData.address1)
+                    .setState(testData.state)
+                    .setCity(testData.city)
+                    .submitForm();
+        });
+        step("Проверяем введённые данные в итоговой форме", () -> {
+            registrationPage.checkResultTableOpened()
+                    .checkSubmittedValue("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkSubmittedValue("Student Email", testData.userEmail)
+                    .checkSubmittedValue("Gender", testData.gender)
+                    .checkSubmittedValue("Mobile", testData.mobile)
+                    .checkSubmittedValue("Date of Birth",
+                            testData.birthDay + " " + testData.birthMonth + "," + testData.birthYear)
+                    .checkSubmittedValue("Subjects", String.join(", ", testData.selectedSubjects))
+                    .checkSubmittedValue("Hobbies", String.join(", ", testData.selectedHobbies))
+                    .checkSubmittedValue("Picture", testData.picture1)
+                    .checkSubmittedValue("Address", testData.address1)
+                    .checkSubmittedValue("State and City", testData.state + " " + testData.city);
+        });
     }
 
     @Test
+    @Tag("Positive")
+    @DisplayName("Успешное заполнение только обязательных полей регистрационной формы")
     void regFormFillMinSuccessTest() {
-
-        registrationPage.openPage()
-                .removeBanners()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setGender(testData.gender)
-                .setMobile(testData.mobile)
-                .submitForm();
-
-        registrationPage.checkResultTableOpened()
-                .checkSubmittedValue("Student Name", testData.firstName + " " + testData.lastName)
-                .checkSubmittedValue("Gender", testData.gender)
-                .checkSubmittedValue("Mobile", testData.mobile);
-
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> registrationPage.openPage());
+        step("Удаляем мешающие баннеры", () -> registrationPage.removeBanners());
+        step("Заполняем обязательные поля регистрационной формы", () -> {
+            registrationPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setGender(testData.gender)
+                    .setMobile(testData.mobile)
+                    .submitForm();
+        });
+        step("Проверяем введённые данные в итоговой форме", () -> {
+            registrationPage.checkResultTableOpened()
+                    .checkSubmittedValue("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkSubmittedValue("Gender", testData.gender)
+                    .checkSubmittedValue("Mobile", testData.mobile);
+        });
     }
 
     @Test
+    @Tag("Negative")
+    @DisplayName("Негативный тест без заполнения каких-либо полей регистрационной формы")
     void regFormAllBlankNegativeTest() {
-
-        registrationPage.openPage()
-                .removeBanners()
-                .submitForm();
-
-        registrationPage.checkResultTableNotOpened();
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> registrationPage.openPage());
+        step("Удаляем мешающие баннеры", () -> registrationPage.removeBanners());
+        step("Нажимаем кнопку 'Submit'", () -> registrationPage.submitForm());
+        step("Проверяем, что итоговая форма не открылась", () -> registrationPage.checkResultTableNotOpened());
     }
 
     @Test
+    @Tag("Negative")
+    @DisplayName("Негативный тест, не все обязательные поля регистрационной формы были заполнены")
     void regFormFillNotAllNecessaryFieldsNegativeTest() {
-
-        registrationPage.openPage()
-                .removeBanners()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setEmail(testData.userEmail)
-                .setDateOfBirth(testData.birthDay, testData.birthMonth, testData.birthYear)
-                .setSubjects(testData.selectedSubjects)
-                .setHobbies(testData.selectedHobbies)
-                .setAddress(testData.address1)
-                .setState(testData.state)
-                .setCity(testData.city)
-                .submitForm();
-
-        registrationPage.checkResultTableNotOpened();
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> registrationPage.openPage());
+        step("Удаляем мешающие баннеры", () -> registrationPage.removeBanners());
+        step("Заполняем некоторые поля регистрационной формы, не все обязательные", () -> {
+            registrationPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setEmail(testData.userEmail)
+                    .setDateOfBirth(testData.birthDay, testData.birthMonth, testData.birthYear)
+                    .setSubjects(testData.selectedSubjects)
+                    .setHobbies(testData.selectedHobbies)
+                    .setAddress(testData.address1)
+                    .setState(testData.state)
+                    .setCity(testData.city)
+                    .submitForm();
+        });
+        step("Проверяем, что итоговая форма не открылась", () -> registrationPage.checkResultTableNotOpened());
     }
 }
--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -2,21 +2,35 @@ package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.demoqa.pages.helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        //Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1280x960";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
     void afterEach() {
+        Attach.screenshotAs("Last_screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         Selenide.closeWebDriver();
     }
 }
